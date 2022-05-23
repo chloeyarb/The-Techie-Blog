a3a55ef38d8dafdb3e62d94835332bdda167e07e
Chloe Yarborough <cmyarboro@gmail.com>
2022-05-23 08:14:12 -0400

    updated login/signup functionality

--- public/javascript/signup.js
+++ public/javascript/signup.js
@@ -1,6 +1,24 @@
 function signupFormHandler(event) {
     event.preventDefault();
-}
 
+    const username = document.querySelector('#username-signup').value.trim();
+    const password = document.querySelector('#password-signup').value.trim();
+
+    if (username && password) {
+        const response = await fetch('/api/users', {
+            method: 'POST',
+            body: JSON.stringify({
+                username,
+                password
+            }),
+            headers: { 'Content-Type': 'application/json' }
+        });
+        if (response.ok) {
+            document.location.replace('/dashboard/');
+        } else {
+            alert(response.statusText);
+        }
+    }
+}
 // 14.2.4
 document.querySelector('.signup-form').addEventListener('submit', signupFormHandler);
